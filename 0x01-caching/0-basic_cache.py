23b59988c57bb40bcb08482af435b6f8a85e273e
YemiDev <abodunrinsulaimon1@gmail.com>
2023-08-23 23:32:54 +0100

    A Caching System Class That Inherited From BaseCache Class:

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,39 +1,11 @@
 #!/usr/bin/env python3
-""" BaseCaching module is a caching system class
-    with subclass that inherit from it
+""" BasicCaching Class is a caching system class
+    subclass that inherits from BaseCaching Parent
+    Class
 """
 
 
-class BaseCaching():
-    """ BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """ Initiliaze
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
-    def put(self, key, item):
-        """ Add an item in the cache
-        """
-        raise NotImplementedError(
-            "put must be implemented in your cache class")
-
-    def get(self, key):
-        """ Get an item by key
-        """
-        raise NotImplementedError(
-            "get must be implemented in your cache class")
+BaseCaching = __import__("basecaching").BaseCaching
 
 
 class BasicCache(BaseCaching):
@@ -41,33 +13,29 @@ class BasicCache(BaseCaching):
         from BaseChasing as seen
     """
 
+    def __init__(self):
+        """ Initialization """
+        super().__init__()
+
     def put(self, key, item):
         """ Add an item in the cache
         """
         self.key = key
         self.item = item
-        try:
-            if self.key is None:
-                return
+        if self.key is None:
+            return
 
-            new_data = {self.key: self.item}
-            new_data[self.key] = self.item
-            return self.cache_data.update(new_data)
-            # self.cache_data.append(new_data)
-        except Exception:
-            raise NotImplementedError(
-                "put must be implemented in your cache class")
+        new_data = {self.key: self.item}
+        new_data[self.key] = self.item
+        return self.cache_data.update(new_data)
+        # self.cache_data.append(new_data)
 
     def get(self, key):
         """ Get an item by key
         """
         self.key = key
-        try:
-            if self.key is None or self.key not in self.cache_data:
-                return None
+        if self.key is None or self.key not in self.cache_data:
+            return None
 
-            for keys in self.cache_data:
-                return self.cache_data[key]
-        except Exception:
-            raise NotImplementedError(
-                "get must be implemented in your cache class")
+        for keys in self.cache_data:
+            return self.cache_data[self.key]
