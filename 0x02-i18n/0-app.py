0ab3b5233a3528bdd9d65333d0a6604927c204eb
YemiDev <abodunrinsulaimon1@gmail.com>
2023-08-30 00:08:58 +0100

    Config Class With Language Translation Added

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,11 +4,21 @@
 
 
 from flask import Flask, render_template
+from flask_babel import Babel
+import datetime
+import pytz
 
 
 app = Flask(__name__)
 app.strict_slashes = False
+bable = Babel(app)
 
+
+class Config():
+    """ Language Translation Class """
+    LANGUAGES = ["en", "fr"]
+    locale = "en"
+    time_zone = datetime.datetime.now(pytz.utc)
 
 @app.route("/")
 def index():
