693a94117c4d98c3527c6c273672ea28914316e6
YemiDev <abodunrinsulaimon1@gmail.com>
2023-08-30 17:44:55 +0100

    Setting Time Zone and Initializing Babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+""" A flask web application for translation
+"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+from flask_babel import _
+import datetime
+import pytz
+
+
+app = Flask(__name__)
+app.strict_slashes = False
+# babel = Babel(app)
+
+
+class Config():
+    """ Language Translation Class """
+    LANGUAGES = ["en", "fr"]
+    locale = "en"
+    time_zone = datetime.datetime.now(pytz.utc)
+    babel = Babel(app)
+
+@app.route("/")
+def index_one():
+    """ App Index page """
+    locale = Config.locale
+    time_zone = Config.time_zone
+    return render_template("1-index.html", locale=locale, time_zone=time_zone)
+
+if __name__ == "__main__":
+    app.run(port=5050)
