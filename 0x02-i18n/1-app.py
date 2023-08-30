364fd72ff96442654e70b1a49af9ec4d908172d4
YemiDev <abodunrinsulaimon1@gmail.com>
2023-08-30 18:45:07 +0100

    Instantiated Babel, Config Class and UTC Time Zone Set

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -18,8 +18,8 @@ babel = Babel(app)
 class Config():
     """ Language Translation Class """
     LANGUAGES = ["en", "fr"]
-    default_locale = "en"
-    default_timezone = "UTC"
+    locale = "en"
+    timezone = "UTC"
     time_zone = datetime.datetime.now(pytz.utc)
     # babel = Babel(app)
 
