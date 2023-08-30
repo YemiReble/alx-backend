91f02379cc6d28dc5740b11d51a4b6d2ecdf15a7
YemiDev <abodunrinsulaimon1@gmail.com>
2023-08-30 18:57:50 +0100

    Laguage Selector [Best for user]

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" A flask web application for translation
+"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, format_datetime
+from flask_babel import _
+import datetime
+import pytz
+
+
+app = Flask(__name__)
+app.strict_slashes = False
+babel = Babel(app)
+
+
+class Config():
+    """ Configuration Class """
+    
+    LANGUAGES = ["en", "fr"]
+    locale = "en"
+    timezone = "UTC"
+    time_zone = datetime.datetime.now(pytz.utc)
+    # babel = Babel(app)
+
+
+@app.route("/")
+def index_one():
+    """ App Index page """
+    
+    locale = Config.locale
+    time_zone = format_datetime(Config.time_zone)
+    return render_template("1-index.html", locale=locale, time_zone=time_zone)
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(port=5050)
